5d8c89ce46eac6346f07807f80abdfcd40422e9e
onokusuna <piroshki4315@gmail.com>
2020-10-29 21:54:46 +0900

    Modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -44,9 +45,14 @@ public class IndexServlet extends HttpServlet {
          */
         //getWriterは、文字テキストをクライアントに送ることができるPrintWriterオブジェクトを戻す。
         //appendは、Char型の文字かCharSequence型の文字列の値を、現在の文字列の後に追加する。
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        //response.getWriter().append(Integer.valueOf(messages.size()).toString());
 
         em.close();
+
+        request.setAttribute("messages", messages);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
 	}
 
 }
