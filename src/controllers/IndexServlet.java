44e9a079b3a21436e01ab63aa10ea17895545dd3
onokusuna <piroshki4315@gmail.com>
2020-10-30 15:24:25 +0900

    Add pagination

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -37,19 +37,27 @@ public class IndexServlet extends HttpServlet {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
         EntityManager em = DBUtil.createEntityManager();
 
+        // 開くページ数を取得（デフォルトは1ページ目）
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        // 最大件数と開始位置を指定してメッセージを取得
         List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
+                                   .setFirstResult(15 * (page - 1))
+                                   .setMaxResults(15)
                                    .getResultList();
-        /*
-         * サーブレットの処理結果としてなんらかの文字を出力する
-         * にはgetWriterという名称のメソッドを使用する必要がある。
-         */
-        //getWriterは、文字テキストをクライアントに送ることができるPrintWriterオブジェクトを戻す。
-        //appendは、Char型の文字かCharSequence型の文字列の値を、現在の文字列の後に追加する。
-        //response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        // 全件数を取得
+        long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class)
+                                      .getSingleResult();
 
         em.close();
 
         request.setAttribute("messages", messages);
+        request.setAttribute("messages_count", messages_count);     // 全件数
+        request.setAttribute("page", page);                         // ページ数
 
         // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除）
